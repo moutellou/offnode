3241326af727f3f4db3b2f2e119ea61c18105fac
moh <moutellou@gmail.com>
2015-02-27 22:02:02 -0500

    first run

--- offnode-client/target/classes/org/myapp/App.java
+++ offnode-client/target/classes/org/myapp/App.java
@@ -41,7 +41,7 @@ public class App implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("GWT User 1");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
